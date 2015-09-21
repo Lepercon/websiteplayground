742b521b1d67e14cc5d0e1fd28a593558b3a7263
Rupert Maspero <rmaspero@gmail.com>
2015-09-21 09:47:39 +0100

    Addition of Volunteering Page
    Added volunteering pages and content

---
+++ application/views/volunteering/content.php
@@ -0,0 +1,15 @@
+<h2>Projects Committee.</h2>
+<p>Projects Committee is the name given to Butlers committee that helps run and find new ways in which students from Butler can give back to the communities around them. Students give up their time all year round to work with our Projects Comm. volunteering initiatives. Projects are a really great way to make a difference in the local community. We’ve got a really successful scheme with a local primary school, and so many more volunteering opportunities! Interested? If you have any projects you are passionate about or would like to start up please contact either your Vice-President or the College Chaplain.</p>
+
+<h2>Our Projects</h2>
+<p>These are just some of our wonderful volunteering opportunities that you can get involved with during your time at Butler.</p>
+<h3>St. Joseph’s</h3>
+<p>We’ve got volunteering links with St. Joseph’s Primary School in Gilesgate, where our students run after-school clubs, help with reading, coach sports and even put on an annual activity day! If you are interested please contact either the Vice-President or St. Josephs Rep.</p>
+<h3>Moving On</h3>
+<p>Moving On is a charity for homeless youths in County Durham. We are looking for volunteers wanting to help and empathise with people as they go through this process. Contact Vice-President if interested.</p>
+<h3>ITV</h3>
+<p>Interested in diversity in television? Students volunteer to sit on an ITV panel looking at diversity in ITV’s programming. Contact the College Chaplin if you are interested.</p>
+<h3>Haggrid</h3>
+<p>Haggrid is a scheme based around horticulture and agriculture working with children who are seen as being at high risk of being involved in crime. Butler students are helping them put together an independent review of the scheme. Want in? Contact the College Chaplin.</p>
+<h3>A Way Out</h3>
+<p>They deliver services to women and young people. Their work spans basic outreach, drugs prevention and alternative education to help people live healthy, happy and whole lives. Want to be involved? Contact the College Chaplin.</p>
