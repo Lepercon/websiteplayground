3689349c84c42327eec0e61292eb8174fe74499a
alex-naylor <a.j.naylor@durham.ac.uk>
2015-10-08 12:32:24 +0100

    fixing bokkings
    index chanhed and disable previous booking page

--- application/views/bookings/home.php
+++ application/views/bookings/home.php
@@ -2,7 +2,7 @@
 	echo 'Welcome to the Butler JCR bookings page!<br><br>
 		Please be aware that availabilty on here may not be 100% up to date and that bookings take 2 working days to be processed by college.<br><br>
 		You cannot make a booking which crosses between term and holiday time.<br><br>
-		The University and Josephine Butler College reserve the right to reject any booking applications and to overwrite any accepted bookings at a later date.';
-	echo '<br><br><br>'.anchor('bookings/book_old', 'Old system', 'class="jcr-button"');
+		The University and Josephine Butler College reserve the right to reject any booking applications and to overwrite any accepted bookings at a later date.'.'<br><br><br>';
+	//echo anchor('bookings/book_old', 'Old system', 'class="jcr-button"');
 	echo ' '.anchor('bookings/calender', 'View availability', 'class="jcr-button"');
 	echo ' '.anchor('bookings/book', 'Make a booking', 'class="jcr-button"');
