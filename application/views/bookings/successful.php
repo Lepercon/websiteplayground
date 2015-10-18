4720d1a8e8071611cee492c930bc0fd801bb1085
alex-naylor <a.j.naylor@durham.ac.uk>
2015-10-18 16:51:56 +0100

    testing equiptment for bookings

--- application/views/bookings/successful.php
+++ application/views/bookings/successful.php
@@ -1,9 +1,11 @@
 <?php
 
 echo 'Your request has been submitted to the College staff and should be prosesed within 2 working days. You will recieve email confirmation once this is done.<br><br>
-	Please be aware that all bookings are subject to being overwritten by the University. You will recieve an email at the start of every week you have a booking detailing whether any changes have been made
+	Please be aware that all bookings are subject to being overwritten by the University. You will recieve an email at the start of every week you have a booking detailing whether any changes have been made<br><br>
 	If you have any queeries please click '.anchor('contact/user/jcr', 'HERE').' or contact: butler.jcr@durham.ac.uk<br><br>
 	Thank you for booking through the JCR website!';
 	
-	echo '<br><br><br>'.anchor('bookings/index', 'Return to bookings home', 'class="jcr-button"'); 
-
+echo '<br><br><br>'.anchor('bookings/index', 'Return to bookings home', 'class="jcr-button"');
+        
+        
+echo '<br><br>'."<b>".$this->bookings_model->euiptment_id_2_name($b['Equiptment'])."</b>";
