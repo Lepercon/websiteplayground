08e6fdd71ff19f9ffedb9b1755a73cd33647c801
samstradling <samuel.stradling@durham.ac.uk>
2015-11-05 21:04:54 +0000

    Excel Reading Update

--- application/views/bookings/read_excel.php
+++ application/views/bookings/read_excel.php
@@ -1,9 +1,21 @@
 <?php
-
-    $file = 'application/views/bookings/files/test.xlsx';
-    $objPHPExcel = PHPExcel_IOFactory::load($file);
-    
+    $path = 'application/views/bookings/files/';
+    $file = '4 Nov 15 KX.xlsx';
+    $objReader = PHPExcel_IOFactory::createReader('Excel2007');
+    $objPHPExcel = $objReader->load($path.$file);
+    /*
+    $sheetNames = $objPHPExcel->getSheetNames();
     echo '<pre>';
-    $sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
-    var_dump($sheetData);
+    foreach($sheetNames as $sheetIndex => $sheetName) {
+            echo 'WorkSheet #',$sheetIndex,' is named "',$sheetName,'"<br />';
+            $objPHPExcel->setActiveSheetIndexByName($sheetName);
+            $sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
+            var_dump($sheetData);
+    }
     echo '</pre>';
+     */
+    $objPHPExcel->setActiveSheetIndexByName("Conference Room Plan");
+    var_dump($objPHPExcel->getActiveSheet()->getComment('D4')->getText()->getPlainText());
+    $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+    $objWriter->Save($path.'TTest.xlsx');
+    
