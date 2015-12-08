1640653689ab7b679cd55f06bd57c8feb7c66cc6
alex-naylor <a.j.naylor@durham.ac.uk>
2015-12-08 23:14:47 +0000

    fix

--- application/models/bookings_model.php
+++ application/models/bookings_model.php
@@ -53,22 +53,22 @@ class Bookings_model extends CI_Model {
                 if(preg_match($re, $this->input->post('last_date'), $e_date)){
                     $end = $this->epoch_convert($e_hour, $e_min, $e_date);
                 }
-<<<<<<< HEAD
-                $equipt = $this->input->post('equipt')!==FALSE?$this->input->post('equipt'):array();
-		$equiptment = implode(', ', array_keys($equipt));
-		$details = array(
-			'user_id' => $this->session->userdata('id'),
-                        'room_id' => $this->input->post('room_id'),
-			'booking_start' => $start,
-			'booking_end' => $end,
-			'frequency' => $this->input->post('Frequency_of_bookings'),
-			'number_of_people' => $this->input->post('Number_of_People'),
-			'Phone_number' => $this->input->post('Phone_number'),
-			'Title' => $this->input->post('Title'),
-                        'Equiptment' => $equiptment,
-                        'Layout' => $this->input->post('Room_Layout')==FALSE?' ':$this->input->post('Room_Layout'),
-		);
-		return $details;	
+            }
+            $equipt = $this->input->post('equipt')!==FALSE?$this->input->post('equipt'):array();
+            $equiptment = implode(', ', array_keys($equipt));
+            $details = array(
+                    'user_id' => $this->session->userdata('id'),
+                    'room_id' => $this->input->post('room_id'),
+                    'booking_start' => $start,
+                    'booking_end' => $end,
+                    'frequency' => $this->input->post('Frequency_of_bookings'),
+                    'number_of_people' => $this->input->post('Number_of_People'),
+                    'Phone_number' => $this->input->post('Phone_number'),
+                    'Title' => $this->input->post('Title'),
+                    'Equiptment' => $equiptment,
+                    'Layout' => $this->input->post('Room_Layout')==FALSE?' ':$this->input->post('Room_Layout'),
+            );
+            return $details;	
 	}
 	
 	function get_room_id(){
@@ -142,127 +142,8 @@ class Bookings_model extends CI_Model {
 		$this->email->send();
 	}
         
-        function check_clash ($details){
-            $ins = $this->db->get('bookings_instances')->result_array();
-            $rooms = $this->db->get('bookings_rooms')->result_array();
-            $instances = array();
-            foreach ($rooms as $r){
-                    $instances[$r['id']] = array();
-=======
->>>>>>> refs/remotes/origin/master
-            }
-            if ($this->epoch_convert(0, 0, $s_date) > $this->epoch_convert(0, 0, $e_date)){
-                $GLOBALS['errors']['date'] = 'Start date after end date';   
-            }
-            if ($start > $this->epoch_convert($e_hour, $e_min, $s_date)){  
-                    $GLOBALS['errors']['time'] = 'Start time after end time';
-            }
-            $equipt = $this->input->post('equipt')!==FALSE?$this->input->post('equipt'):array();
-            $equiptment = implode(', ', array_keys($equipt));
-            $details = array(
-                    'user_id' => $this->session->userdata('id'),
-                    'room_id' => $this->input->post('room_id'),
-                    'booking_start' => $start,
-                    'booking_end' => $end,
-                    'frequency' => $this->input->post('Frequency_of_bookings'),
-                    'number_of_people' => $this->input->post('Number_of_People'),
-                    'Phone_number' => $this->input->post('Phone_number'),
-                    'Title' => $this->input->post('Title'),
-                    'Equiptment' => $equiptment,
-                    'Layout' => $this->input->post('Room_Layout')==FALSE?' ':$this->input->post('Room_Layout'),
-            );
-            return $details;	
-    }
 
-    function get_room_id(){
-            $details = array('room_id' => $this->input->post('room_id'));
-            return $details;
-    }
 
-    function enter_data($details){
-            $this->db->insert('bookings_reservations', $details);
-            $id = $this->db->insert_id();
-
-            $times = array();
-            switch($details['frequency']){
-                    case 0:
-                            $times = array($details['booking_start']);
-                            break;
-                    case 1:
-                            for($t = $details['booking_start']; $t <= $details['booking_end']; $t+=60*60*24*7){
-                                    $times[] = $t;
-                            }
-                            break;
-                    case 2:
-                            for($t = $details['booking_start']; $t <= $details['booking_end']; $t+=60*60*24*14){
-                                    $times[] = $t;
-                            }
-                            break;
-                    case 3:
-                            for($t = $details['booking_start']; $t <= $details['booking_end']; $t+=60*60*24*28){
-                                    $times[] = $t;
-                            }
-                            break;
-            }
-            $booking_len = (($details['booking_end'] - $details['booking_start']) % (60*60*24));
-            $instance = array();
-            foreach ($times as $t){
-                    $end_t = $t + $booking_len;
-                    $instance[] = array('time_start' => $t,
-                                                    'time_end' => $end_t,
-                                                    'booking_id'=> $id,
-                                                    'room_id' => $details['room_id']
-                                                    );
-            }
-            $this->db->insert_batch('bookings_instances', $instance);
-            return $id;
-    }
-
-    function room_id_to_name($details){
-        $this ->db->where('id',$details['room_id']);
-        $r = $this->db->get('bookings_rooms')->row_array();
-        return $r['name'];
-    }
-
-    function euiptment_id_2_name($id) {
-        $this ->db->where('id',$id);
-        $equipment = $this->db->get('bookings_equiptment')->row_array();
-        return $equipment['e_name'];
-    }
-
-    function send_email($message, $details){
-            $room = $this->room_id_to_name($details);
-            $this->load->library('email');
-            $config['mailtype'] = 'html';
-            $config['wordwrap'] = FALSE;    
-            $this->email->initialize($config);
-            $this->email->from($this->session->userdata('email'), $this->session->userdata('firstname').' '.$this->session->userdata('surname'));
-            $this->email->to('butler.reception@durham.ac.uk'); 
-            //$this->email->cc('a.j.naylor@durham.ac.uk'); 
-            $this->email->bcc('a.j.naylor@durham.ac.uk');
-            $this->email->subject('Room booking - '.$room.date(' - l',$details['booking_start']));
-            $this->email->message($message); 
-            $this->email->send();
-    }
-
-    function check_clash ($details){
-        $ins = $this->db->get('bookings_instances')->result_array();
-        $rooms = $this->db->get('bookings_rooms')->result_array();
-        $instances = array();
-        foreach ($rooms as $r){
-                $instances[$r['id']] = array();
-        }
-        foreach ($ins as $i){
-            if ($i['room_id'] == $this->input->post('room_id')){
-                if ($i['time_start'] < $details['booking_start'] && $details['booking_start'] < $i['time_end']){
-                    $GLOBALS['errors'][] = 'The start time clashes with another bookings. Please view the availabilty page to see when the clash is';
-                }
-                if ($i['time_start'] < $details['booking_end'] && $details['booking_end'] < $i['time_end']){
-                    $GLOBALS['error'][] = 'The end time clashes with another bookings. Please view the availabilty page to see when the clash is';
-                }    
-            }
-        }
-<<<<<<< HEAD
         function room_name2id($name){
             
             $this->db->like('spreadsheet_name', $name, 'none');
@@ -271,12 +152,8 @@ class Bookings_model extends CI_Model {
             if(!empty($room))
                 return $room['id'];
             return NULL;
-        }
-=======
-        $booking_screen = TRUE;
-        return $booking_screen;
+        
     }
->>>>>>> refs/remotes/origin/master
 }
 
 
